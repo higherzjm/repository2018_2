17640ca41b79c2b469dae65486082b85af1e6227
higherzjm <353263668@qq.com>
2018-08-20 14:51:35 +0800

    新增  Test_20180820

---
+++ gradleproject2.0/java/Test_20180820.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by zjm on 2018/8/20.
+ */
+public class Test_20180820 {
+
+    @Test
+    public void test1(){
+        System.out.println("-----------------------------------");
+    }
+}
