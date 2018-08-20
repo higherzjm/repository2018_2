7ecb8d7a8de4d3d8a65ba49f6d8abfd58c32fca9
higherzjm <353263668@qq.com>
2018-08-20 14:48:04 +0800

    更新 ClassloaderTest

--- gradleproject2.0/java/ClassloaderTest.java
+++ gradleproject2.0/java/ClassloaderTest.java
@@ -1,8 +1,15 @@
+import org.junit.Test;
+
 public class ClassloaderTest
 {
   public static void test1(String paramstr,int paramint)
   {
     System.out.println("ClassloaderTest 测试1--->paramstr:"+paramstr+";paramint:"+paramint);
+  }
+
+  @Test
+  public void test2(){
+    System.out.print("2222");
   }
 
 }
