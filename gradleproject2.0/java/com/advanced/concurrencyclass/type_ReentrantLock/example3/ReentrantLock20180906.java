9c985f1c758811a573119ccfd3b4cbc89b17373c
higherzjm <353263668@qq.com>
2018-09-13 16:02:20 +0800

    更新2018-09-13

--- gradleproject2.0/java/com/advanced/concurrencyclass/type_ReentrantLock/example3/ReentrantLock20180906.java
+++ gradleproject2.0/java/com/advanced/concurrencyclass/type_ReentrantLock/example3/ReentrantLock20180906.java
@@ -1,8 +1,6 @@
 package com.advanced.concurrencyclass.type_ReentrantLock.example3;
 
-import org.junit.Test;
 
-import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -12,8 +10,6 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ReentrantLock20180906 {
     Lock lock=new ReentrantLock();
-    final Condition conditionA=lock.newCondition();
-    final Condition conditionB=lock.newCondition();
 
     public static void main(String[] args){
         ReentrantLock20180906 reentrantLock20180906=new ReentrantLock20180906();
@@ -25,8 +21,17 @@ public class ReentrantLock20180906 {
      * 按正常如果线程1没有释放锁，线程2锁会获取不成功
      */
     public void test1(){
-        new Thread(new Thread1()).start();
-        /*new Thread(new Thread2()).start();*/
+         Thread thread1=new Thread(new Thread1());
+         thread1.setName("thread1");
+         thread1.start();
+
+         Thread thread2=new Thread(new Thread2());
+         thread2.setName("thread2");
+         thread2.start();
+
+         Thread thread3=new Thread(new Thread3());
+         thread3.setName("thread3");
+         thread3.start();
     }
 
     class Thread1 implements Runnable{
@@ -36,9 +41,7 @@ public class ReentrantLock20180906 {
             System.out.println("thread1 tryLock:"+lock.tryLock());
             lock.lock();
             System.out.println("thread1 获取锁成功");
-
-
-
+            lock.unlock();
         }
     }
 
@@ -50,6 +53,16 @@ public class ReentrantLock20180906 {
             lock.lock();
             System.out.println("thread2 获取锁成功");
 
+        }
+    }
+
+    class Thread3 implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("thread3 tryLock:"+lock.tryLock());
+            lock.lock();
+            System.out.println("thread3 获取锁成功");
         }
     }
 }
