087247d98389f3f1097aa73df588e1205ef028aa
higherzjm <353263668@qq.com>
2019-01-21 09:37:02 +0800

    更新2019-01-21

--- gradleproject2.0/java/com/j2ee/spring/spring_aop/example3_execution_jackson/AspectJackson.java
+++ gradleproject2.0/java/com/j2ee/spring/spring_aop/example3_execution_jackson/AspectJackson.java
@@ -1,11 +1,9 @@
 package com.j2ee.spring.spring_aop.example3_execution_jackson;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
 @Aspect
 @Component("aspectJackson")
 public class AspectJackson {
