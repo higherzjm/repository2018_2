ba4f12854053d85d8cca948c771febbaa213a6ed
higherzjm <353263668@qq.com>
2019-04-12 17:20:31 +0800

    更新2019-04-12

--- gradleproject2.0/java/com/j2ee/email/MailSummaryUtil.java
+++ gradleproject2.0/java/com/j2ee/email/MailSummaryUtil.java
@@ -1,14 +1,13 @@
 package com.j2ee.email;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.*;
 
 public class MailSummaryUtil {
 
@@ -17,7 +16,7 @@ public class MailSummaryUtil {
         Properties properties = new Properties();
         properties.put("mail.transport.protocol", "smtp");// 连接协议
         properties.put("mail.smtp.host", "smtp.qq.com");// 主机名
-        properties.put("mail.smtp.port", 465);// 端口号
+        properties.put("mail.smtp.port", 465);// 端口号465
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.ssl.enable", "true");// 设置是否使用ssl安全连接 ---一般都使用
         properties.put("mail.debug", "true");// 设置是否显示debug信息 true 会在控制台显示相关信息
@@ -28,8 +27,8 @@ public class MailSummaryUtil {
         // 设置发件人邮箱地址
         message.setFrom(new InternetAddress("353263668@qq.com"));
         // 设置收件人邮箱地址
-        message.setRecipients(Message.RecipientType.TO, new InternetAddress[]{new InternetAddress("1045174284@qq.com"),new InternetAddress("353263668@qq.com"),new InternetAddress("zhujianming@paymier.com")});
-        //message.setRecipient(Message.RecipientType.TO, new InternetAddress("xxx@qq.com"));//一个收件人
+        //message.setRecipients(Message.RecipientType.TO, new InternetAddress[]{new InternetAddress("1045174284@qq.com"),new InternetAddress("353263668@qq.com"),new InternetAddress("zhujianming@paymier.com")});
+        message.setRecipient(Message.RecipientType.TO, new InternetAddress("zhujianming@paymier.com"));//一个收件人
         // 设置邮件标题
         message.setSubject("姜老板你好我是zjm");
         // 设置邮件内容
@@ -37,10 +36,74 @@ public class MailSummaryUtil {
         // 得到邮差对象
         Transport transport = session.getTransport();
         // 连接自己的邮箱账户,密码为QQ邮箱开通的stmp服务后得到的客户端授权码
-        transport.connect("353263668@qq.com", "wisbgbfslkzobiab");//POP3/SMTP服务
+        //transport.connect("353263668@qq.com", "wisbgbfslkzobiab");//POP3/SMTP服务
         transport.connect("353263668@qq.com", "snkvocvkotvdbibi");//IMAP/SMTP服务:
         // 发送邮件
         transport.sendMessage(message, message.getAllRecipients());
         transport.close();
     }
+    static String HOST = ""; // smtp服务器
+    static String FROM = ""; // 发件人地址
+    static String TO = ""; // 收件人地址
+    static String AFFIX = ""; // 附件地址
+    static String AFFIXNAME = ""; // 附件名称
+    static String USER = ""; // 用户名
+    static String PWD = ""; // 163的授权码
+    static String SUBJECT = ""; // 邮件标题
+    static String[] TOS = null;
+    @Test
+    public void test163Email(){
+            try {
+                Properties props = new Properties();
+                props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));//从自定义配置文件获取相关参数
+                HOST = props.getProperty("host");
+                FROM = props.getProperty("from");
+                TO = props.getProperty("to");
+                TOS = TO.split(",");
+                AFFIX = props.getProperty("affix");
+                AFFIXNAME = props.getProperty("affixName");
+                USER = props.getProperty("user");
+                PWD = props.getProperty("pwd");
+                SUBJECT = props.getProperty("subject");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Properties props = new Properties();
+            props.put("mail.smtp.host", HOST);//设置发送邮件的邮件服务器的属性（这里使用网易的smtp服务器）
+            props.put("mail.smtp.auth", "true");  //需要经过授权，也就是有户名和密码的校验，这样才能通过验证（一定要有这一条）
+            Session session = Session.getDefaultInstance(props);//用props对象构建一个session
+            session.setDebug(true);
+            MimeMessage message = new MimeMessage(session);//用session为参数定义消息对象
+            try {
+                message.setFrom(new InternetAddress(FROM));// 加载发件人地址
+                InternetAddress[] sendTo = new InternetAddress[TOS.length]; // 加载收件人地址
+                for (int i = 0; i < TOS.length; i++) {
+                    sendTo[i] = new InternetAddress(TOS[i]);
+                }
+                message.addRecipients(Message.RecipientType.TO,sendTo);
+                message.addRecipients(MimeMessage.RecipientType.CC, InternetAddress.parse(FROM));//设置在发送给收信人之前给自己（发送方）抄送一份，不然会被当成垃圾邮件，报554错
+                message.setSubject(SUBJECT);//加载标题
+                Multipart multipart = new MimeMultipart();//向multipart对象中添加邮件的各个部分内容，包括文本内容和附件
+                BodyPart contentPart = new MimeBodyPart();//设置邮件的文本内容
+                contentPart.setText("姜老板你好，我是朱建明");
+                multipart.addBodyPart(contentPart);
+                if(!AFFIX.isEmpty()){//添加附件
+                    BodyPart messageBodyPart = new MimeBodyPart();
+                    DataSource source = new FileDataSource(AFFIX);
+                    messageBodyPart.setDataHandler(new DataHandler(source));//添加附件的内容
+                    sun.misc.BASE64Encoder enc = new sun.misc.BASE64Encoder();//添加附件的标题
+                    messageBodyPart.setFileName("=?GBK?B?"+ enc.encode(AFFIXNAME.getBytes()) + "?=");
+                    multipart.addBodyPart(messageBodyPart);
+                }
+                message.setContent(multipart);//将multipart对象放到message中
+                message.saveChanges(); //保存邮件
+                Transport transport = session.getTransport("smtp");//发送邮件
+                transport.connect(HOST, USER, PWD);//连接服务器的邮箱
+                transport.sendMessage(message, message.getAllRecipients());//把邮件发送出去
+                transport.close();//关闭连接
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
 }
